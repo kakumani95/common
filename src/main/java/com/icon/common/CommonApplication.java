47eae9bf5ca4e161d163d293ed41d76ae8286ca3
kakumani95 <venkatanaveen495@gmail.com>
2019-08-25 16:23:48 +0530

    employee get and post calls

--- src/main/java/com/icon/common/CommonApplication.java
+++ src/main/java/com/icon/common/CommonApplication.java
@@ -2,8 +2,10 @@ package com.icon.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.icon.common")
 public class CommonApplication {
 
 	public static void main(String[] args) {
